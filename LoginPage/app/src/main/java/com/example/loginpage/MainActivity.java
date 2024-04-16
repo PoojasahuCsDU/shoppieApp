6575458524aa3b617c0aadeffdb5fca1983f0f05
deepak-kumar <deepakvickykumar360@gmail.com>
2024-04-16 22:48:53 +0530

    developed splash screen

---
+++ LoginPage/app/src/main/java/com/example/loginpage/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.loginpage;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
